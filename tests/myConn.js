032d72c106f43fefdd5d060220e37d417113ca77
张添 <zhangtian1104@gmail.com>
2016-08-30 16:09:28 +1000

    connect to mysql

---
+++ tests/myConn.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '0427',
+  database : 'test'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * from user', function(err, rows, fields) {
+  if (err) throw err;
+  console.log(rows);
+});
+ 
+connection.end();
